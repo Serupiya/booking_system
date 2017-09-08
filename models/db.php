7104ccb282e7b588629ee10e14cdf79054411b13
Michal Stusek <stusekmichal@gmail.com>
2017-09-08 12:53:46 +0200

    added heroku db

--- models/db.php
+++ models/db.php
@@ -5,6 +5,9 @@
  * Date: 3. 9. 2017
  * Time: 20:47
  */
+/*
+ * localhost
+
 function get_db_connection(){
     $server_name = "localhost";
     $username = "root";
@@ -13,4 +16,17 @@ function get_db_connection(){
     $conn = new mysqli($server_name, $username, $password);
     return $conn;
 }
+*/
 
+//heroku
+function get_db_connection(){
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = new mysqli($server, $username, $password, $db);
+    return $conn;
+}
