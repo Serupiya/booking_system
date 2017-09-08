d92b650d988d60f70690917e57365ec60ea9dfad
Michal Stusek <stusekmichal@gmail.com>
2017-09-08 12:47:28 +0200

    fixed error

--- templates/js/loading_screen.js
+++ templates/js/loading_screen.js
@@ -29,7 +29,8 @@ function get_config(retry, status_div){
         url: "/views/config/get_config.php",
         success: function (data) {
             if (data["error"] !== undefined) {
-                show_error_dialog(data["error"]);
+                status_div.append($("<span style='color: indianred'>" + project_arr["error"] + "</span>"));
+                load_fail();
             } else {
                 for (var key in data){
                     config[key] = data[key];
