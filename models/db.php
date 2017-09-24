7fdc735f72bb410fc0f3dbc474c451c1e45248da
Michal Stusek <stusekmichal@gmail.com>
2017-09-24 19:40:17 +0200

    fix

--- models/db.php
+++ models/db.php
@@ -7,7 +7,7 @@
  */
 
 //localhost
-
+/*
 function get_db_connection(){
     $server_name = "localhost";
     $username = "root";
@@ -21,11 +21,11 @@ function get_db_connection(){
 function get_schema_name(){
     return "booking_system";
 }
+*/
 
 
 
 
-/*
 //heroku
 function get_db_connection(){
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -44,4 +44,3 @@ function get_schema_name(){
     return "heroku_426923c18edf463";
 
 }
-*/
